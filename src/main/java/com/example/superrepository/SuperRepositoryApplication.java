090649b25d172e1a077ef878287e681fee6f7caf
HongSeok Ju <wnghdtjr129@naver.com>
2023-10-07 23:59:26 +0900

    first commit

---
+++ src/main/java/com/example/superrepository/SuperRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.superrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperRepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperRepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/superrepository/SuperRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
